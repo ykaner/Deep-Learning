fa58845bce0236141ef4f2b4424a4f75a7c76d2e
olicht <29300900+olicht@users.noreply.github.com>
2018-05-29 12:42:57 +0300

    args fixed

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
-import hw2.cifar10_network
 import argparse
+
+import hw2.cifar10_network
 
 parser = argparse.ArgumentParser()
 
-parser.add_argument('-load', const=True, default=False, nargs='?',
+parser.add_argument('--load', const=True, default=False, nargs='?',
                     help='if set loads the saved model')
-parser.add_argument('-epochs', type=int, default=100, nargs='?',
+parser.add_argument('--epochs', type=int, default=100, nargs='?',
                     help='the number of training epochs')
-parser.add_argument('-gpus', type=int, default=4, nargs='?',
+parser.add_argument('--gpus', type=int, default=4, nargs='?',
                     help='the number of gpus you want use')
 
 args = parser.parse_args()
