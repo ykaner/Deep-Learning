17e120897720033614348b3880a3f6a75f317a45
Yosef Kaner <ykaner12@gmail.com>
2018-05-30 20:38:24 +0300

    print(pred_class)

--- hw2/cifar10_network.py
+++ hw2/cifar10_network.py
@@ -367,136 +367,6 @@ def maybe_download_and_extract():
 		os.remove(zip_cifar_10)
 
 
-def theirs_train():
-	"""Train CIFAR-10 for a number of steps."""
-	with tf.Graph().as_default(), tf.device('/cpu:0'):
-		# Create a variable to count the number of train() calls. This equals the
-		# number of batches processed * FLAGS.num_gpus.
-		global_step = tf.get_variable(
-				'global_step', [],
-				initializer=tf.constant_initializer(0), trainable=False)
-		
-		# Calculate the learning rate schedule.
-		num_batches_per_epoch = (cifar10.NUM_EXAMPLES_PER_EPOCH_FOR_TRAIN /
-		                         FLAGS.batch_size)
-		
-		# decay_steps = int(num_batches_per_epoch * cifar10.NUM_EPOCHS_PER_DECAY)
-		
-		# Decay the learning rate exponentially based on the number of steps.
-		# lr = tf.train.exponential_decay(cifar10.INITIAL_LEARNING_RATE,
-		#                                 global_step,
-		#                                 decay_steps,
-		#                                 cifar10.LEARNING_RATE_DECAY_FACTOR,
-		#                                 staircase=True)
-		
-		# Create an optimizer that performs gradient descent.
-		# opt = tf.train.GradientDescentOptimizer(lr)
-		
-		# Get images and labels for CIFAR-10.
-		images, labels = cifar10.distorted_inputs()
-		batch_queue = tf.contrib.slim.prefetch_queue.prefetch_queue(
-				[images, labels], capacity=3 * FLAGS.num_gpus)
-		# Calculate the gradients for each model tower.
-		tower_grads = []
-		with tf.variable_scope(tf.get_variable_scope()):
-			for i in range(FLAGS.num_gpus):
-				with tf.device('/gpu:%d' % i):
-					with tf.name_scope('%s_%d' % (cifar10.TOWER_NAME, i)) as scope:
-						# Dequeues one batch for the GPU
-						image_batch, label_batch = batch_queue.dequeue()
-						# Calculate the loss for one tower of the CIFAR model. This function
-						# constructs the entire CIFAR model but shares the variables across
-						# all towers.
-						loss = tower_loss(scope, image_batch, label_batch)
-						
-						# Reuse variables for the next tower.
-						tf.get_variable_scope().reuse_variables()
-						
-						# Retain the summaries from the final tower.
-						summaries = tf.get_collection(tf.GraphKeys.SUMMARIES, scope)
-						
-						# Calculate the gradients for the batch of data on this CIFAR tower.
-						grads = opt.compute_gradients(loss)
-						
-						# Keep track of the gradients across all towers.
-						tower_grads.append(grads)
-		
-		# We must calculate the mean of each gradient. Note that this is the
-		# synchronization point across all towers.
-		grads = average_gradients(tower_grads)
-		
-		# Add a summary to track the learning rate.
-		summaries.append(tf.summary.scalar('learning_rate', lr))
-		
-		# Add histograms for gradients.
-		for grad, var in grads:
-			if grad is not None:
-				summaries.append(tf.summary.histogram(var.op.name + '/gradients', grad))
-		
-		# Apply the gradients to adjust the shared variables.
-		apply_gradient_op = opt.apply_gradients(grads, global_step=global_step)
-		
-		# Add histograms for trainable variables.
-		for var in tf.trainable_variables():
-			summaries.append(tf.summary.histogram(var.op.name, var))
-		
-		# Track the moving averages of all trainable variables.
-		variable_averages = tf.train.ExponentialMovingAverage(
-				cifar10.MOVING_AVERAGE_DECAY, global_step)
-		variables_averages_op = variable_averages.apply(tf.trainable_variables())
-		
-		# Group all updates to into a single train op.
-		train_op = tf.group(apply_gradient_op, variables_averages_op)
-		
-		# Create a saver.
-		saver = tf.train.Saver(tf.global_variables())
-		
-		# Build the summary operation from the last tower summaries.
-		summary_op = tf.summary.merge(summaries)
-		
-		# Build an initialization operation to run below.
-		init = tf.global_variables_initializer()
-		
-		# Start running operations on the Graph. allow_soft_placement must be set to
-		# True to build towers on GPU, as some of the ops do not have GPU
-		# implementations.
-		sess = tf.Session(config=tf.ConfigProto(
-				allow_soft_placement=True,
-				log_device_placement=FLAGS.log_device_placement))
-		sess.run(init)
-		
-		# Start the queue runners.
-		tf.train.start_queue_runners(sess=sess)
-		
-		summary_writer = tf.summary.FileWriter(FLAGS.train_dir, sess.graph)
-		
-		for step in range(FLAGS.max_steps):
-			start_time = time.time()
-			_, loss_value = sess.run([train_op, loss])
-			duration = time.time() - start_time
-			
-			assert not np.isnan(loss_value), 'Model diverged with loss = NaN'
-			
-			if step % 10 == 0:
-				num_examples_per_step = FLAGS.batch_size * FLAGS.num_gpus
-				examples_per_sec = num_examples_per_step / duration
-				sec_per_batch = duration / FLAGS.num_gpus
-				
-				format_str = ('%s: step %d, loss = %.2f (%.1f examples/sec; %.3f '
-				              'sec/batch)')
-				print(format_str % (datetime.now(), step, loss_value,
-				                    examples_per_sec, sec_per_batch))
-			
-			if step % 100 == 0:
-				summary_str = sess.run(summary_op)
-				summary_writer.add_summary(summary_str, step)
-			
-			# Save the model checkpoint periodically.
-			if step % 1000 == 0 or (step + 1) == FLAGS.max_steps:
-				checkpoint_path = os.path.join(FLAGS.train_dir, 'model.ckpt')
-				saver.save(sess, checkpoint_path, global_step=step)
-
-
 def train(epoch):
 	"""
 	Train the network.
@@ -561,6 +431,8 @@ def test_and_save(epoch):
 		# test_writer.add_run_metadata(run_metadata, "epoch{}:step{}".format(epoch, i))
 		i = j
 	
+	print('predicted class:')
+	print(predicted_class)
 	correct = (np.argmax(test_y, axis=1) == predicted_class)
 	acc = correct.mean() * 100
 	correct_numbers = correct.sum()
