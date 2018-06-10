6e2e4095bf7d62f6452a4a00f34e81c2806a28f3
olicht <29300900+olicht@users.noreply.github.com>
2018-06-10 11:51:56 +0300

    imports fix

--- hw2/cifar10.py
+++ hw2/cifar10.py
@@ -37,18 +37,18 @@ import re
 import sys
 import tarfile
 
-import cifar10_input
+from . import cifar10_input
 import tensorflow as tf
 from six.moves import urllib
 
-FLAGS = tf.app.flags.FLAGS
+FLAGS = tf.flags.FLAGS
 
 # Basic model parameters.
-tf.app.flags.DEFINE_integer('batch_size', 128,
+tf.flags.DEFINE_integer('batch_size', 128,
                             """Number of images to process in a batch.""")
-tf.app.flags.DEFINE_string('data_dir', '/tmp/cifar10_data',
+tf.flags.DEFINE_string('data_dir', '/tmp/cifar10_data',
                            """Path to the CIFAR-10 data directory.""")
-tf.app.flags.DEFINE_boolean('use_fp16', False,
+tf.flags.DEFINE_boolean('use_fp16', False,
                             """Train the model using fp16.""")
 
 # Global constants describing the CIFAR-10 data set.
